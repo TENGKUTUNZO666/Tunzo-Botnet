cced2d31dbd8e995ab4ce374e8d8319a210fd84b
TENGKUTUNZO666 <138317603+TENGKUTUNZO666@users.noreply.github.com>
2023-08-11 10:09:06 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+<div align=center>
+ 
+# 🚀 VyXDDoS : Release v5.1 - Privat DDoS Panel 🚀
+
+# README ♥️
+Thank you for using, please help me press a star button, thank you very much.<br>
+One star = continuously updating multiple methods
+
+# Info
+- [x] Open Source
+- [x] Powerful
+- [x] Simple
+- [x] Methods for Layer 4 and 7
+- [x] Bypass (Cloudflare, OVH, NFO,...)  
---
+++ installer.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+
+os.system('apt-get install nodejs -y ')
+os.system('apt-get install screen -y ')
+os.system('apt-get install perl -y')
+os.system('apt-get install golang -y')
+os.system('apt install npm')
+os.system('pip install colorama')
+os.system('pip install requests')
+os.system('apt-get install python3 -y')
+os.system('pip install undetected_chromedriver')
+os.system('npm install fs')
+os.system('npm install url')
+os.system('npm install net')
+os.system('npm install eval')
+os.system('npm install cloudscraper')
+os.system('chmod +x *')
+print(f"INSTALL SELESAI") 
+os.system('python3 cnc.py')
